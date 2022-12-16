61945e20652a0f409f06aeb8e94d745bc1fb308f
David Wang <wangd7@bxscience.edu>
2022-12-15 21:37:26 -0500

    Tested linter on files and modified some rules

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1>atomhacks is based</h1> 
+      <h1>atomhacks is based</h1>
     </div>
-  )
+  );
 }
