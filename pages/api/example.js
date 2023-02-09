93a701a384802bcb2c4ef6a75d9a4648da2fbeae
warren yun <wyun2006@gmail.com>
2023-02-09 12:34:38 -0500

    basic example create request

--- pages/api/example.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  console.log(req.body.name)
-  res.status(200).json({ message: "slay" })
-}
-
---
+++ pages/api/example/[id].js
@@ -0,0 +1,12 @@
+import prisma from "../../../lib/prisma"
+
+export default async (req, res) => {
+
+  switch(req.method) {
+    case "GET":
+      break;
+    case "DELETE":
+      break;
+  }
+
+}
