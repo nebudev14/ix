013b0c1c7713246ef88f2213d656c8966cdb5b05
Fahim Hussain <fahimhussain21@gmail.com>
2023-02-18 16:38:51 -0500

    fix navbar overlap

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
 import "../styles/globals.css";
 
-import NavBar from '../components/NavBar'
+import NavBar from "../components/NavBar";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <SessionProvider session={session}>
-        <Component {...pageProps} />
+        <NavBar />
+        <div className="content">
+          <Component {...pageProps} />
+        </div>
       </SessionProvider>
-      <NavBar />
     </>
-
   );
 }
 
