949b0b27436cc38730c198fc9318162a0ccb82db
David Wang <wangd7@bxscience.edu>
2023-02-18 14:50:40 -0500

    Minor rewrite of index.js, will fix layout later

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,11 @@
 import "../styles/globals.css";
 
 import { SessionProvider } from "next-auth/react";
-import { AnimatePresence, motion } from "framer-motion";
 
-function MyApp({ Component, pageProps: { session, ...pageProps }, router }) {
-  const variants = {
-    hidden: { opacity: 0, x: -200, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -100 },
-  };
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <div className="bg-slate-100">
-        <AnimatePresence mode="wait" />
-        <motion.div
-          initial="hidden"
-          animate="enter"
-          exit="exit"
-          variants={variants}
-          transition={{ duration: 0.6 }}
-          key={router.asPath}
-        >
-          <Component {...pageProps} />
-        </motion.div>
-      </div>
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,30 +1,28 @@
-import Image from 'next/image';
-import Sample from '../public/atomhackspic.JPG'
+import Image from "next/image";
+import Sample from "../public/atomhackspic.JPG";
 
 function About() {
-    return(
-        <div class="container mx-auto py-40 flex gap-20">
-            <div class="relative">
-                <img class="h-1/4 absolute top-0 left-0 -z-10" src="public/atom.png" alt=""></img>
-                <div class="h-full rounded-full">
-                    <Image src={Sample}></Image>
-                </div>
-                <h1>Left (img area)</h1>
-            </div>
-
-            <div class="my-auto flex flex-col gap-3">
-                <h1 class="mb-2 text-5xl font-thin"> ABOUT ATOMHACKS </h1>
-                <h1 class="text-md font-thin"> AtomHacks is committed to creating and organizing innovative and interactive coding competitions for the Bronx High School <br/> of Science. We are driven to give back to our community and provide transformative computer science opportunities for students <br/> of all levels.
-
- </h1>
-            </div>
-            
-
+  return (
+    <div class="container mx-auto py-40 flex gap-20">
+      <div class="relative">
+        <Image class="h-1/4 absolute top-0 left-0 -z-10" src="public/atom.png" alt=""></Image>
+        <div class="h-full rounded-full">
+          <Image src={Sample} alt=""></Image>
         </div>
-    )
-    
+        <h1>Left (img area)</h1>
+      </div>
+
+      <div class="my-auto flex flex-col gap-3">
+        <h1 class="mb-2 text-5xl font-thin"> ABOUT ATOMHACKS </h1>
+        <h1 class="text-md font-thin">
+          {" "}
+          AtomHacks is committed to creating and organizing innovative and interactive coding competitions for the Bronx
+          High School <br /> of Science. We are driven to give back to our community and provide transformative computer
+          science opportunities for students <br /> of all levels.
+        </h1>
+      </div>
+    </div>
+  );
 }
 
-
- 
 export default About;
