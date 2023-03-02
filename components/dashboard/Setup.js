07b2318ef41cdfc2a1217cacec5c9b68bf0983b6
Fahim Hussain <fahimhussain21@gmail.com>
2023-03-02 18:08:00 -0500

    all options added with state and client side validation

--- components/dashboard/Setup.js
+++ components/dashboard/Setup.js
@@ -5,8 +5,53 @@ import { Transition, Dialog, RadioGroup } from "@headlessui/react";
 // TODO: client side validation
 export default function Setup({ setInitialized }) {
   const [isOpen, setIsOpen] = useState(true);
+  const [osis, setOsis] = useState("");
   const [experience, setExperience] = useState("BEGINNER");
+  const [year, setYear] = useState("");
+  const [confirmation, setConfirmation] = useState("NO");
+  const [discord, setDiscord] = useState("");
+  const [hasTeam, setHasTeam] = useState(undefined);
+  const [team, setTeam] = useState("");
+  const [shouldMatch, setShouldMatch] = useState(undefined);
   const experienceLevels = ["Beginner", "Intermediate", "Advanced"];
+  const graduationYears = ["2023", "2024", "2025", "2026"];
+  const confirmations = ["YES", "NO"];
+
+  // We gotta use Formik or something
+  const isValid = () => {
+    return (
+      experience &&
+      !isNaN(year) &&
+      confirmation == "YES" &&
+      osis.length == 9 &&
+      !isNaN(osis) &&
+      validUsername(discord) &&
+      validTeamSetup()
+    );
+  };
+
+  const validTeamSetup = () => {
+    if (hasTeam === undefined) {
+      return false;
+    }
+    if (hasTeam === false && shouldMatch === undefined) {
+      return false;
+    }
+    if (hasTeam === true && team.length == 0) {
+      return false;
+    }
+    return true;
+  };
+
+  const validUsername = (username) => {
+    if (username.length == 0) {
+      return false;
+    }
+    const regex = /^.{2,32}#\d{4}$/;
+    return regex.test(username);
+  };
+
+  // to be updated
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(e.target.osis);
@@ -66,15 +111,17 @@ export default function Setup({ setInitialized }) {
                     First-time Setup
                   </Dialog.Title>
                   <div className="mt-2 text-neutral-300">
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit} className="space-y-4">
                       <label className="block text-base text-neutral-400" htmlFor="osis">
                         OSIS:
                       </label>
                       <input
                         className="mt-1 mb-4 block text-xl p-2 rounded-md bg-neutral-700 focus:outline-none shadow-lg focus:ring focus:border-teal-600 focus:ring-teal-500"
                         type="text"
+                        value={osis}
                         id="osis"
                         name="osis"
+                        onInput={(e) => setOsis(e.target.value)}
                         autoComplete="OSIS"
                       />
                       <RadioGroup value={experience} onChange={setExperience}>
@@ -99,10 +146,162 @@ export default function Setup({ setInitialized }) {
                           ))}
                         </div>
                       </RadioGroup>
+                      <RadioGroup value={year} onChange={setYear}>
+                        <RadioGroup.Label>
+                          <p className="mb-2 text-neutral-400">Graduation Year</p>
+                        </RadioGroup.Label>
+                        <div className="space-y-2">
+                          {graduationYears.map((year, index) => (
+                            <RadioGroup.Option
+                              className={({ checked }) =>
+                                `${
+                                  checked ? "bg-teal-600" : "bg-neutral-700"
+                                } cursor-pointer rounded-lg px-4 py-2 shadow-md w-2/5`
+                              }
+                              key={index}
+                              id={year.toLowerCase()}
+                              name="year"
+                              value={+year}
+                            >
+                              <span>{year}</span>
+                            </RadioGroup.Option>
+                          ))}
+                        </div>
+                      </RadioGroup>
+                      <RadioGroup value={confirmation} onChange={setConfirmation}>
+                        <RadioGroup.Label>
+                          <p className="mb-2 text-neutral-400">
+                            Do you agree to the terms of the{" "}
+                            <a
+                              className="underline underline-offset-4 decoration-2 text-green-500"
+                              href="https://docs.google.com/document/d/1fMx-8iApjgRuAs0mH2T4yCz6WGrwTwNGHS854C-fmKQ/edit"
+                            >
+                              Bronx Science AtomHacks Code of Conduct?
+                            </a>{" "}
+                            If you do not agree to these terms, you may not participate in this event.
+                          </p>
+                        </RadioGroup.Label>
+                        <div className="space-y-2">
+                          {confirmations.map((option, index) => (
+                            <RadioGroup.Option
+                              className={({ checked }) =>
+                                `${
+                                  checked ? "bg-teal-600" : "bg-neutral-700"
+                                } cursor-pointer rounded-lg px-4 py-2 shadow-md w-2/5`
+                              }
+                              key={index}
+                              id={option.toLowerCase()}
+                              name="confirmation"
+                              value={option}
+                            >
+                              <span>{option}</span>
+                            </RadioGroup.Option>
+                          ))}
+                        </div>
+                      </RadioGroup>
+                      <div className="bg-neutral-700 rounded-md p-4 space-y-4">
+                        <h1 className="text-2xl">Information Regarding Discord</h1>
+                        <p>
+                          We will be using Discord for communications throughout the hackathon. Discord will be used if
+                          you plan on working in a team, and will also be used to give updates throughout the day. If
+                          you do not have a Discord account, please sign up{" "}
+                          <a
+                            className="underline underline-offset-4 decoration-2 text-green-500"
+                            href="https://discord.com/register"
+                          >
+                            here.
+                          </a>
+                        </p>
+                        <p>Once you make your Discord account, find your name and four-digit number: </p>
+                        <p>
+                          If you are on desktop, you can find your name on the bottom left. Below your name will be #,
+                          followed by your four-digit number. If you are on mobile, click on your profile picture on the
+                          bottom right of the app. On the top should be your name, followed by your four-digit number.
+                        </p>
+                      </div>
+                      <label className="block text-base text-neutral-400" htmlFor="discord">
+                        Discord Username with tag (e.g. Guy#1234)
+                      </label>
+                      <input
+                        className="mt-1 mb-4 block text-xl p-2 rounded-md bg-neutral-700 focus:outline-none shadow-lg focus:ring focus:border-teal-600 focus:ring-teal-500"
+                        type="text"
+                        id="discord"
+                        name="discord"
+                        value={discord}
+                        onInput={(e) => setDiscord(e.target.value)}
+                      />
+                      <RadioGroup value={hasTeam} onChange={setHasTeam}>
+                        <RadioGroup.Label>
+                          <p className="mb-2 text-neutral-400">Do you have a team?</p>
+                        </RadioGroup.Label>
+                        <div className="space-y-2">
+                          {/* LOL */}
+                          {[true, false].map((option, index) => (
+                            <RadioGroup.Option
+                              className={({ checked }) =>
+                                `${
+                                  checked ? "bg-teal-600" : "bg-neutral-700"
+                                } cursor-pointer rounded-lg px-4 py-2 shadow-md w-2/5`
+                              }
+                              key={index}
+                              id={option ? "yes" : "no"}
+                              name="hasTeam"
+                              value={option}
+                            >
+                              <span>{option ? "Yes" : "No"}</span>
+                            </RadioGroup.Option>
+                          ))}
+                        </div>
+                      </RadioGroup>
+                      {typeof hasTeam === "boolean" && hasTeam === true && (
+                        <>
+                          <label className="block text-base text-neutral-400" htmlFor="team">
+                            Who is on your team? (Doesn't have to be final)
+                          </label>
+                          <input
+                            className="mt-1 mb-4 block text-xl p-2 rounded-md bg-neutral-700 focus:outline-none shadow-lg focus:ring focus:border-teal-600 focus:ring-teal-500"
+                            type="text"
+                            id="team"
+                            name="team"
+                            value={team}
+                            onInput={(e) => setTeam(e.target.value)}
+                          />
+                        </>
+                      )}
+                      {typeof hasTeam === "boolean" && hasTeam === false && (
+                        <>
+                          <RadioGroup value={shouldMatch} onChange={setShouldMatch}>
+                            <RadioGroup.Label>
+                              <p className="mb-2 text-neutral-400">
+                                Would you like us to match you with a team? (We will match you with others who do not
+                                have a team)
+                              </p>
+                            </RadioGroup.Label>
+                            <div className="space-y-2">
+                              {[true, false].map((option, index) => (
+                                <RadioGroup.Option
+                                  className={({ checked }) =>
+                                    `${
+                                      checked ? "bg-teal-600" : "bg-neutral-700"
+                                    } cursor-pointer rounded-lg px-4 py-2 shadow-md w-2/5`
+                                  }
+                                  key={index}
+                                  id={option ? "yes" : "no"}
+                                  name="hasTeam"
+                                  value={option}
+                                >
+                                  <span>{option ? "Yes" : "No"}</span>
+                                </RadioGroup.Option>
+                              ))}
+                            </div>
+                          </RadioGroup>
+                        </>
+                      )}
                       <div className="mt-4">
                         <button
                           type="submit"
-                          className="transition duration-200 inline-flex justify-center rounded-md border border-transparent bg-teal-500 px-4 py-2 text-sm font-medium text-white hover:bg-teal-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          disabled={isValid() ? false : true}
+                          className="transition duration-200 inline-flex justify-center rounded-md border border-transparent bg-teal-500 px-4 py-2 text-sm font-medium text-white hover:bg-teal-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:opacity-50 disabled:bg-teal-600"
                           onClick={() => setIsOpen(false)}
                         >
                           Submit
