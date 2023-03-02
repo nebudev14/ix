9c73fccf3677a1f1d23930c4013c71b3888f3ef4
warren yun <wyun2006@gmail.com>
2023-03-02 13:22:40 -0500

    goofy

--- components/landing/sponsor.js
+++ components/landing/sponsor.js
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
+const list = [
+  {
+    name: "Small Planet",
+    path: "sp-logo.svg",
+    link: "",
+  },
+  {
+    name: "Echo3D",
+    path: "echo3d-logo.png",
+    link: "",
+  },
+];
+
 const Sponsors = () => {
   return (
     <div id="#sponsors" className="flex flex-col items-center justify-center py-24 border-b-8 border-green-500 ">
@@ -5,6 +20,13 @@ const Sponsors = () => {
         <span className="py-6 border-b-4 border-yellow-500 text-7xl font-morro">SPONSORS</span>
       </div>
       <h1 className="mb-20 text-xl font-montserrat">Thanks to our amazing sponsors for making AtomHacks possible!</h1>
+      <div className="flex flex-wrap items-center justify-center">
+        {list.map((sponsor, i) => (
+          <div className="mx-12 my-8">
+            <Image width={250} height={250} key={i} src={`/assets/sponsors/${sponsor.path}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
